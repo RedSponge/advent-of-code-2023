ce7a52401773bfedb8e80ee23e3bf595facb692f
RedSponge <eran.godsi2003@gmail.com>
2023-12-01 23:20:57 +0200

    Day 1 Part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,17 +1,55 @@
 use std::fs;
 
-fn get_calibration_value(line: &str) -> Option<u32> {
-    let mut iter = line.chars().filter(|ch| ch.is_digit(10));
-    let first_digit = iter.next()?;
-    let last_digit = iter.last().or(Some(first_digit))?;
+const DIGIT_MAPPING: [(&str, u32); 18] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5),
+    ("6", 6),
+    ("7", 7),
+    ("8", 8),
+    ("9", 9),
+];
 
-    let first_digit = first_digit
-        .to_digit(10)
-        .expect("Filter should've made this impossible!");
-    let last_digit = last_digit
-        .to_digit(10)
-        .expect("Filter should've made this impossible!");
-    return Some(first_digit * 10 + last_digit);
+fn find_digit(mut line: &str, scan_forwards: bool) -> Option<u32> {
+    while !line.is_empty() {
+        let found = DIGIT_MAPPING.iter().find(|(text, _digit)| {
+            if scan_forwards {
+                line.starts_with(text)
+            } else {
+                line.ends_with(text)
+            }
+        });
+
+        if let Some((_text, digit)) = found {
+            return Some(*digit);
+        }
+
+        if scan_forwards {
+            line = &line[1..]
+        } else {
+            line = &line[..line.len() - 1];
+        }
+    }
+
+    None
+}
+
+fn get_calibration_value(line: &str) -> Option<u32> {
+    let first_digit = find_digit(line, true)?;
+    let last_digit = find_digit(line, false)?;
+
+    Some(first_digit * 10 + last_digit)
 }
 
 fn get_calibration_sum(text: &str) -> Option<u32> {
@@ -38,13 +76,25 @@ mod tests {
         assert_eq!(get_calibration_value("a1b2c3d4e5f"), Some(15));
         assert_eq!(get_calibration_value("treb7uchet"), Some(77));
         assert_eq!(get_calibration_value("trebuchet"), None);
+        assert_eq!(get_calibration_value("two1nine"), Some(29));
+        assert_eq!(get_calibration_value("eightwothree"), Some(83));
+        assert_eq!(get_calibration_value("abcone2threexyz"), Some(13));
+        assert_eq!(get_calibration_value("xtwone3four"), Some(24));
+        assert_eq!(get_calibration_value("4nineeightseven2"), Some(42));
+        assert_eq!(get_calibration_value("zoneight234"), Some(14));
+        assert_eq!(get_calibration_value("7pqrstsixteen"), Some(76));
+        assert_eq!(get_calibration_value("three"), Some(33));
+        assert_eq!(
+            get_calibration_value("seven8sevenptdlvvgssixvjvzpvsp7fivefourtwoned"),
+            Some(71)
+        );
     }
 
     #[test]
     fn test_get_calibration_sum() {
         assert_eq!(
             get_calibration_sum(&fs::read_to_string("example.txt").unwrap()),
-            Some(142)
+            Some(281)
         );
     }
 }
