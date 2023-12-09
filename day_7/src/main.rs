c3eb0915873e97865561d361fea353708eca3ff2
RedSponge <eran.godsi2003@gmail.com>
2023-12-09 22:45:51 +0200

    Day 7 Part 2

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -3,9 +3,32 @@ use std::{collections::HashMap, fs};
 const HAND_SIZE: usize = 5;
 
 #[derive(PartialEq, Eq, Debug)]
-struct Hand([u32; HAND_SIZE]);
+struct Hand([CardValue; HAND_SIZE]);
+
+#[derive(Eq, PartialEq, PartialOrd, Ord, Debug)]
+enum CardValue {
+    Joker,
+    Number(u32),
+}
+
+impl CardValue {
+    fn from_char(ch: char) -> Self {
+        match ch {
+            'J' => Self::Joker,
+            'A' => Self::Number(14),
+            'K' => Self::Number(13),
+            'Q' => Self::Number(12),
+            'T' => Self::Number(10),
+            other => Self::Number(other.to_digit(10).unwrap() as u32),
+        }
+    }
+}
 
 fn get_distinct_counts(vals: &[u32]) -> Vec<usize> {
+    if vals.len() == 0 {
+        return vec![0];
+    }
+
     let mut collector: HashMap<u32, usize> = HashMap::default();
     vals.iter().for_each(|&v| {
         collector
@@ -54,20 +77,26 @@ impl HandVariation {
 
 impl Hand {
     fn variation(&self) -> HandVariation {
-        HandVariation::from_distinct_counts(&get_distinct_counts(&self.0))
+        let mut numbers = vec![];
+        let mut jokers = 0;
+
+        for val in &self.0 {
+            match val {
+                CardValue::Number(n) => numbers.push(*n),
+                CardValue::Joker => jokers += 1,
+            }
+        }
+
+        let mut counts = get_distinct_counts(&numbers);
+        counts.sort();
+        *counts.last_mut().unwrap() += jokers;
+        HandVariation::from_distinct_counts(&counts)
     }
 
     fn parse(s: &str) -> Self {
         Self(
             s.chars()
-                .map(|ch| match ch {
-                    'A' => 14,
-                    'K' => 13,
-                    'Q' => 12,
-                    'J' => 11,
-                    'T' => 10,
-                    other => other.to_digit(10).unwrap(),
-                })
+                .map(CardValue::from_char)
                 .collect::<Vec<_>>()
                 .try_into()
                 .unwrap(),
@@ -77,7 +106,7 @@ impl Hand {
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (self.variation(), self.0).cmp(&(other.variation(), other.0))
+        (self.variation(), &self.0).cmp(&(other.variation(), &other.0))
     }
 }
 
@@ -118,6 +147,9 @@ mod tests {
 
     use super::*;
 
+    type CV = CardValue;
+    type HV = HandVariation;
+
     fn assert_eq_sorted(a: &[usize], b: &[usize]) {
         let mut a = Vec::from_iter(a.iter());
         let mut b = Vec::from_iter(b.iter());
@@ -135,56 +167,56 @@ mod tests {
 
     #[test]
     fn test_hand_variation_from_counts() {
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[5]),
-            HandVariation::FiveOAK
-        );
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[1, 4]),
-            HandVariation::FourOAK
-        );
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[3, 2]),
-            HandVariation::FullHouse
-        );
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[3, 1, 1]),
-            HandVariation::ThreeOAK
-        );
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[2, 1, 2]),
-            HandVariation::TwoPairs
-        );
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[1, 1, 1, 2]),
-            HandVariation::OnePair
-        );
-        assert_eq!(
-            HandVariation::from_distinct_counts(&[1, 1, 1, 1, 1]),
-            HandVariation::HighCard
-        );
+        assert_eq!(HV::from_distinct_counts(&[5]), HV::FiveOAK);
+        assert_eq!(HV::from_distinct_counts(&[1, 4]), HV::FourOAK);
+        assert_eq!(HV::from_distinct_counts(&[3, 2]), HV::FullHouse);
+        assert_eq!(HV::from_distinct_counts(&[3, 1, 1]), HV::ThreeOAK);
+        assert_eq!(HV::from_distinct_counts(&[2, 1, 2]), HV::TwoPairs);
+        assert_eq!(HV::from_distinct_counts(&[1, 1, 1, 2]), HV::OnePair);
+        assert_eq!(HV::from_distinct_counts(&[1, 1, 1, 1, 1]), HV::HighCard);
     }
 
     #[test]
     fn test_hand_comparison() {
-        assert_eq!(Hand([3, 2, 10, 3, 13]), Hand([3, 2, 10, 3, 13]));
+        assert_eq!(Hand::parse("32T3K"), Hand::parse("32T3K"));
         // Test hand variation precedence
-        assert!(Hand([2, 2, 2, 2, 3]) > Hand([3, 3, 4, 4, 5]));
+        assert!(Hand::parse("22223") > Hand::parse("33445"));
 
         // Test value precedence
-        assert!(Hand([3, 2, 2, 2, 2]) > Hand([2, 2, 2, 2, 3]));
+        assert!(Hand::parse("32222") > Hand::parse("22223"));
+
+        // Test Joker is weakest
+        assert!(Hand::parse("22222") > Hand::parse("JJJJJ"));
     }
 
     #[test]
     fn test_parse_hand() {
-        assert_eq!(Hand::parse("AK9TQ"), Hand([14, 13, 9, 10, 12]));
+        assert_eq!(
+            Hand::parse("AK9TQ"),
+            Hand([
+                CV::Number(14),
+                CV::Number(13),
+                CV::Number(9),
+                CV::Number(10),
+                CV::Number(12)
+            ])
+        );
     }
 
     #[test]
     fn test_find_total_winnings() {
         assert_eq!(
             find_total_winnings(&fs::read_to_string("example.txt").unwrap()),
-            6440
+            5905
         );
+    }
+
+    #[test]
+    fn test_hand_variation_with_jokers() {
+        assert_eq!(Hand::parse("JJJJJ").variation(), HV::FiveOAK);
+        assert_eq!(Hand::parse("JJQJJ").variation(), HV::FiveOAK);
+        assert_eq!(Hand::parse("1234J").variation(), HV::OnePair);
+        assert_eq!(Hand::parse("1334J").variation(), HV::ThreeOAK);
+        assert_eq!(Hand::parse("4334J").variation(), HV::FullHouse);
     }
 }
